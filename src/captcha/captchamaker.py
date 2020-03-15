293b8061352c8fb1b11c3ecbdc755611eb2e4ac4
hexchan <46650621+hexchan@users.noreply.github.com>
2020-03-15 20:42:03 +0300

    Better captcha (#40)
    * Changed captcha generation algo
    
    * Captcha tweaks

--- src/captcha/captchamaker.py
+++ src/captcha/captchamaker.py
@@ -10,57 +10,102 @@ from PIL import Image, ImageFont, ImageDraw, ImageFilter, ImageOps
 from captcha.wakabawords import make_word
 
 
-COLOR_MODE = 'L'
+COLOR_MODE = 'RGBA'
 BACKGROUND_COLOR = '#FFF'
 MAIN_COLOR = '#000'
+TRANSPARENT_COLOR = (0, 0, 0, 0)
 
-FONT_SIZE = 30
-SYMBOL_SPACING = -5
-SYMBOL_WIDTH = 15  # If Ubuntu font size 30
-SYMBOL_HEIGHT = 30  # If Ubuntu font size 30
-Y_DEVIATION = 2
+FONT_SIZE = 24
+VERTICAL_LETTER_CROP = 7
+
+BORDER_WIDTH = 1
+CAPTCHA_WIDTH = 192 - 2 * BORDER_WIDTH
+CAPTCHA_HEIGHT = 48 - 2 * BORDER_WIDTH
+
+HORIZONTAL_SHIFT_RATIO = 0.5
+HORIZONTAL_PADDING = 20
+MAX_VERTICAL_SHIFT = 5
+VERTICAL_PADDING = int((CAPTCHA_HEIGHT - FONT_SIZE) / 2)
 
 
 # Load font object
 module_dir = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(module_dir, 'UbuntuMono-R.ttf'), 'rb') as f:
+with open(os.path.join(module_dir, 'OpenSans-Light.ttf'), 'rb') as f:
     font = PIL.ImageFont.truetype(font=f, size=FONT_SIZE)
 
 
-def draw_distored_text(surface, text, left, top):
-    x = left
+def draw_distored_text(image, text, left, top):
+    # Text angle can be random in specified alternating ranges
+    angle_ranges = [(-30, -15), (15, 30)]
+    current_angle_range = 0
+
+    # Intial character coordinates
+    x = left + HORIZONTAL_PADDING
+    padded_top = top + VERTICAL_PADDING
+    y = padded_top
+
+    # Draw text string char by char
     for char in text:
-        y = random.randint(top - Y_DEVIATION, top + Y_DEVIATION)
-        surface.text(xy=(x, y), text=char, fill=MAIN_COLOR, font=font, )
-        x += (SYMBOL_WIDTH + SYMBOL_SPACING)
+        # Get char dimensions
+        char_width, char_height = font.getsize(char)
+
+        # Create new image for char
+        new_image = Image.new(COLOR_MODE, (char_width, char_height), TRANSPARENT_COLOR)
+
+        # Draw text on the char image
+        new_surface = PIL.ImageDraw.Draw(new_image)
+        new_surface.text(xy=(0, 0), text=char, fill=MAIN_COLOR, font=font)
+
+        # Crop image at the top because font leaves too much space
+        new_image = new_image.crop((0, VERTICAL_LETTER_CROP, new_image.width, new_image.height))
+
+        # Rotate char image, with special handling for 'I' and 'J'
+        random_angle = random.randint(*angle_ranges[current_angle_range])
+        new_image = new_image.rotate(random_angle, expand=True, resample=PIL.Image.BICUBIC)
+
+        # Paste char image to the base one, shift it to the left a little to overlap chars
+        x_shift = int(new_image.width * HORIZONTAL_SHIFT_RATIO)
+        image.alpha_composite(new_image, (max(0, x - x_shift), y))
+
+        # Increment horizontal offset (width with shift)
+        x += int(new_image.width - x_shift)
+
+        # Randomize vertical offset
+        y = padded_top + random.randint(-MAX_VERTICAL_SHIFT, MAX_VERTICAL_SHIFT)
+
+        # Switch angle range
+        current_angle_range = 0 if current_angle_range == 1 else 1
 
 
 def draw_test_sheet():
     image = PIL.Image.new(COLOR_MODE, (1920, 1080), BACKGROUND_COLOR)
-    surface = PIL.ImageDraw.Draw(image)
+    image_surface = PIL.ImageDraw.Draw(image)
 
-    for x in range(10):
-        for y in range(20):
-            solution = make_word()
-            draw_distored_text(surface, solution, x * 200, y * 50)
+    for x_num in range(10):
+        for y_num in range(20):
+            # Generate captcha word
+            solution = make_word().upper()
 
-    image = image.filter(PIL.ImageFilter.SHARPEN)
-    image = image.filter(PIL.ImageFilter.FIND_EDGES)
-    image = PIL.ImageOps.invert(image)
+            # Draw captcha word
+            draw_distored_text(image, solution, x_num * CAPTCHA_WIDTH, y_num * CAPTCHA_HEIGHT)
+
+            # Draw a border around captcha
+            image_surface.rectangle(
+                [
+                    x_num * CAPTCHA_WIDTH, y_num * CAPTCHA_HEIGHT,
+                    (x_num + 1) * CAPTCHA_WIDTH, (y_num + 1) * CAPTCHA_HEIGHT,
+                ],
+                outline=MAIN_COLOR,
+                fill=None
+            )
 
     image.show()
 
 
-def draw_single_captcha(image_size=(200, 40)):
+def draw_single_captcha(image_size=(CAPTCHA_WIDTH, CAPTCHA_HEIGHT)):
     image = PIL.Image.new(COLOR_MODE, image_size, BACKGROUND_COLOR)
-    surface = PIL.ImageDraw.Draw(image)
-
-    solution = make_word()
-    draw_distored_text(surface, solution, 10, 5)
-
-    image = image.filter(PIL.ImageFilter.SHARPEN)
-    image = image.filter(PIL.ImageFilter.FIND_EDGES)
-    image = PIL.ImageOps.invert(image)
+    solution = make_word().upper()
+    draw_distored_text(image, solution, 10, 5)
 
     return image, solution
 
