73f7a2ec40b245b4311202deb356fe0d8dffc42b
hexchan <hexchan16@gmail.com>
2021-04-30 15:00:48 +0300

    eslint pre-commit hook

--- src/imageboard/static/imageboard/main.js
+++ src/imageboard/static/imageboard/main.js
@@ -4,45 +4,45 @@ import RefPopup from './scripts/ref_popup';
 import Lightbox from './scripts/lightbox';
 import ImagesWidget from './scripts/images_widget';
 import PostExpander from './scripts/post_expander';
-import FormMover from './scripts/form_mover';
+// import FormMover from './scripts/form_mover';
 import RefInserter from './scripts/ref_inserter';
 import FormHider from './scripts/form_hider';
 import EmailRevealer from './scripts/email_revealer';
 
 // Post and threads hiders
-let threadHider = new Hider({
+new Hider({
     type: 'thread',
     storageKey: 'hiddenThreads',
 });
 
-let postHider = new Hider({
+new Hider({
     type: 'post',
     storageKey: 'hiddenPosts',
 });
 
 // Highlight user's posts and threads
-let hl = new Highlighter();
+new Highlighter();
 
 // Create popup for refs
-let refPopup = new RefPopup();
+new RefPopup();
 
 // Show full images
-let lightbox = new Lightbox();
+new Lightbox();
 
 // Images file input
-let imagesWidget = new ImagesWidget();
+new ImagesWidget();
 
 // Long post expander
-let postExpander = new PostExpander();
+new PostExpander();
 
 // Move posting form with mouse
-// let formMover = new FormMover(document.getElementById('posting-form'));
+// new FormMover(document.getElementById('posting-form'));
 
 // Insert ref when clicking on post number
-let refInserter = new RefInserter();
+new RefInserter();
 
 // Toggle posting form
-let formHider = new FormHider();
+new FormHider();
 
 // Reveal admin email
-let emailRevealer = new EmailRevealer();
+new EmailRevealer();
